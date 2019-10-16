3db7ee4e0086e8f504c21564959d1803c11c4650
Dawid <dawid.kampa@wp.pl>
2019-10-16 15:47:10 +0200

    First commit

---
+++ project/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+    {
+
+    public GameObject player;
+
+    public float zoomSpeed;
+    public float zoomMax;
+    public float zoomMin;
+    public float zoomRoughness;
+
+    public float rotationSpeed;
+    public float smoothFactor;
+
+
+    private float _zoom;
+
+    private Vector3 _cameraOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _zoom = Camera.main.fieldOfView;
+        _cameraOffset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePosition();
+        HandleZoom();
+        //mainCamera.Rota(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+
+    }
+
+    private void HandleZoom()
+    {
+        _zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        _zoom = Mathf.Clamp(_zoom, zoomMax, zoomMin);
+        Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, _zoom, Time.deltaTime * zoomRoughness);
+    }
+
+
+    private void HandlePosition()
+    {
+
+        Quaternion cameraTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+        _cameraOffset = cameraTurnAngle * _cameraOffset;
+
+
+        transform.position = Vector3.Slerp(transform.position, player.transform.position + _cameraOffset ,smoothFactor );
+        transform.LookAt(player.transform.position);
+
+    }
+}
---
+++ project/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody PlayerBody;
+    private Vector3 _direction;
+    private Quaternion _startingRotation;
+    private bool _rotatingToLeft;
+    private bool _rotatingToRight;
+
+    private Camera _playerCamera;
+
+    public float speed;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerCamera = Camera.main;
+        PlayerBody = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PerformMovement()
+    {
+        // reset movement
+        _direction = Vector3.zero;
+
+        // get vertical and horizontal movement input (controller and WASD/ Arrow Keys)
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // base movement on camera
+        Vector3 combinedInput = vertical * _playerCamera.transform.forward + horizontal * _playerCamera.transform.right;
+
+        // normalize so diagonal movement isnt twice as fast
+        _direction = new Vector3(combinedInput.normalized.x, 0, combinedInput.normalized.z);
+
+        // make sure the input doesnt go negative or above 1;
+        float inputAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+
+
+        // rotate player to movement direction
+        Quaternion rot = Quaternion.LookRotation(_direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.fixedDeltaTime * inputAmount * rotationSpeed);
+
+          PlayerBody.velocity = (_direction * speed* inputAmount);
+
+        //_direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+
+        //// The step size is equal to speed times frame time-> angularSpeed * Time.deltaTime
+        //Vector3 newDir = Vector3.RotateTowards(transform.forward, _direction, rotationSpeed * Time.deltaTime, 0.0f);
+        //Debug.DrawRay(transform.position, newDir, Color.red);//?
+
+        //// Move our position a step closer to the target.
+        //transform.rotation = Quaternion.LookRotation(newDir);
+
+        //transform.Translate(_direction * speed * Time.deltaTime);//,Space.World);
+
+
+
+    }
+
+}
